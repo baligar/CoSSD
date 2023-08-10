7fa7c19fad7a6929de4c978831443543e9e80b9c
Shrishail Baligar <shrishail.baligar@gmail.com>
2023-08-10 08:00:27 -0700

    Added updated model w/o any code deleted

--- python/model.py
+++ python/model.py
@@ -95,6 +95,77 @@ class Model(torch.nn.Module):
         return wavs_out, x
 
 
+'''
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        fb = STFTFB(n_filters=256, kernel_size=128, stride=64)
+        self.enc = Encoder(fb)
+        decoder_fb = STFTFB(n_filters=514, kernel_size=128, stride=64)
+        self.dec = Decoder(decoder_fb)
+        self.masker = TDConvNet(in_chan=516, n_src=1)
+
+        self.m = nn.Linear(14, 64)
+        self.m2 = nn.Linear(64, 499)
+        self.dropout = nn.Dropout(0.25)
+        
+        self.c1 = nn.Conv1d(1, 258, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm1d(258)
+        
+        self.c1d_1 = nn.Conv1d(516, 516, kernel_size=3, stride=1, padding=1)
+        self.co_fc_1 = nn.Conv1d(516, 1, kernel_size=3, stride=1, padding=1)
+        
+        self.fc1 = nn.Linear(499, 32)
+        self.fc2 = nn.Linear(32, 1)
+
+        self.dropout_2 = nn.Dropout(p=0.1)
+        self.sigmoid = nn.Sigmoid()
+        
+
+    def forward(self, wav, cond_wav):
+        tf_rep = self.enc(wav)
+
+        m_out = self.dropout(F.relu(self.m(cond_wav)))
+        m2_out = self.dropout(F.relu(self.m2(m_out)))
+        
+        c1_out = F.relu(self.bn1(self.c1(m2_out)))
+                
+        concat_in = torch.cat((tf_rep, c1_out), dim=1)
+        
+        c1d_1_out = F.relu(self.c1d_1(concat_in))
+        masks = self.masker(c1d_1_out)
+        
+        con_usq = c1d_1_out.unsqueeze(1) * masks
+        
+        co_fc_1_out = self.co_fc_1(con_usq.squeeze(1))
+        
+        x = co_fc_1_out.view(-1, 499)
+        x = self.dropout_2(F.relu(self.fc1(x)))
+        x = self.sigmoid(self.fc2(x)) # Pres/abs
+        
+        wavs_out = self.dec(con_usq) # output sep waveform
+        
+        return wavs_out, x
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+cond_conv_tasnet = Model()
+cond_conv_tasnet.to(device)
+
+wav_out, pres_abs = cond_conv_tasnet(
+    wav=torch.randn(10, 1, 32000).to(device), 
+    cond_wav=torch.randn(10, 1, 14).to(device)
+)
+
+print(wav_out.shape, pres_abs.shape)
+
+
+'''
+
+
 print(device)
 
 # Define and forward
